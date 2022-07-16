6779824b825a83447fdd2bea6af145db991ac84d
miris-mimitako <miris.mikitako@gmail.com>
2022-07-16 21:53:00 +0900

    add: main

---
+++ main.py
@@ -0,0 +1,20 @@
+import streamlit as st
+
+
+def main():
+    st.markdown(
+        """
+  # ようこそMiris Mimitakoの国勢調査分析Appへ
+
+  ## Miris Mimitakoとは
+
+  ## 国勢調査の分析結果へ
+
+  リストはこちらにありますので参考としてください。
+  
+  """
+    )
+
+
+if __name__ == "__main__":
+    main()
