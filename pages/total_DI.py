94c725982c3f7754eb074954437688420ad9e705
miris-mimitako <miris.mikitako@gmail.com>
2022-07-16 23:03:23 +0900

    change: some valiable name

--- pages/total_DI.py
+++ pages/total_DI.py
@@ -16,10 +16,10 @@ else:
   API_KEY = st.secrets["API_KEY"]
 
 
-  
+
 url  =  f"http://api.e-stat.go.jp/rest/3.0/app/json/getStatsData?appId={API_KEY}&lang=J&statsDataId=0003348423&metaGetFlg=Y&cntGetFlg=N&explanationGetFlg=Y&annotationGetFlg=Y&sectionHeaderFlg=1&replaceSpChars=0"
 with  urllib.request.urlopen(url)  as  response:
     data  =  response.read()
-d  =  json.loads(data.decode())
+dic_data  =  json.loads(data.decode())
 
 st.write(d)
