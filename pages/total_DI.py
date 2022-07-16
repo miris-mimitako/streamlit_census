7977016f9063afa52378b9de4f3713bcf74a30a3
miris-mimitako <miris.mikitako@gmail.com>
2022-07-16 22:36:05 +0900

    add: seacrets

--- pages/total_DI.py
+++ pages/total_DI.py
@@ -2,7 +2,7 @@ import streamlit as st
 import  urllib
 import  json
 
-
+API_KEY = st.secrets["API_KEY"]
 url  =  f"http://api.e-stat.go.jp/rest/3.0/app/json/getStatsData?appId={API_KEY}&lang=J&statsDataId=0003348423&metaGetFlg=Y&cntGetFlg=N&explanationGetFlg=Y&annotationGetFlg=Y&sectionHeaderFlg=1&replaceSpChars=0"
 with  urllib.request.urlopen(url)  as  response:
     data  =  response.read()
