b330223902ccfbe34d4516bf69c3acd1a33848bc
miris-mimitako <miris.mikitako@gmail.com>
2022-07-17 23:23:43 +0900

    add: graph

--- pages/total_DI.py
+++ pages/total_DI.py
@@ -4,6 +4,8 @@ import json
 from pathlib import Path
 import os
 import pandas as pd
+import plotly.express as px
+import datetime
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,6 +23,10 @@ url = f"http://api.e-stat.go.jp/rest/3.0/app/json/getStatsData?appId={API_KEY}&l
 with urllib.request.urlopen(url) as response:
     data = response.read()
 dic_data = json.loads(data.decode())
+
+# st.write('<style>div.row-widget.stRadio > div{flex-direction:row;justify-content: left;} </style>', unsafe_allow_html=True)
+
+# st.write('<style>div.st-bf{flex-direction:column;} div.st-ag{font-weight:bold;padding-left:2px;}</style>', unsafe_allow_html=True)
 
 # tab, 140 = DI
 
@@ -58,7 +64,7 @@ st.write(df_value)
 
 df_merge = (
     pd.merge(df_value, df_cat01, left_on="@cat01", right_on="@code", how="outer")
-    .rename(columns={"@name": "分野"})
+    .rename(columns={"@name": "分野", "@level": "category_level"})
     .drop(columns=["@tab", "@cat01", "@code", "@parentCode"])
 )
 df_merge = (
@@ -67,4 +73,39 @@ df_merge = (
     .drop(columns=["@cat02", "@code"])
 )
 
+df_merge["date"] = df_merge["@time"].str[:4] + "-" + df_merge["@time"].str[-2:]
+df_merge["date"] = pd.to_datetime(df_merge.date)
+# df_merge["date"] = df_merge["@time"][:4] + "-" + df_merge["@time"][-2:]
+
+df_merge["DI"] = df_merge["$"].astype("float32")
+df_merge["DI_standard"] = df_merge["$"].astype("float32") - 50
+df_merge["category_level"] = df_merge["category_level"].astype("int16")
+df_merge.sort_values("date", inplace=True)
 st.write(df_merge)
+
+
+col1, col2 = st.columns(2)
+
+#
+df_merge_lv3 = df_merge[df_merge.category_level == 3][
+    df_merge["方向及び水準"] == "景気の現状判断（水準）"
+]
+
+min_household_trends_year, max_household_trends_year = st.select_slider(
+    "期間を選択してください。",
+    options=pd.DatetimeIndex(df_merge_lv3.date).year,
+    value=(
+        pd.DatetimeIndex(df_merge_lv3.date).year.min(),
+        pd.DatetimeIndex(df_merge_lv3.date).year.max(),
+    ),
+)
+
+min_household_trends = datetime.date(min_household_trends_year, 1, 1)
+max_household_trends = datetime.date(max_household_trends_year, 12, 31)
+
+
+fig_household_trends = px.line(df_merge_lv3, x="date", y="DI", color="分野")
+fig_household_trends.update_xaxes(
+    title="年月", range=[min_household_trends, max_household_trends]
+)
+st.plotly_chart(fig_household_trends, use_container_width=True)
